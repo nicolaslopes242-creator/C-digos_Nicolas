5f981ff7f56006149c50e6e1f479c4c0b17c0f82
Nicolas <santanalopesnicolas@gmail.com>
2025-11-11 16:55:35 -0300

    Add files via upload

---
+++ Extra/Revisão.py
@@ -0,0 +1,17 @@
+while True:
+    pergunta = input("Qual o número do seu crush ?")
+    if pergunta == "40028922":
+        print("Migeul gatão, imaginei ")
+        break
+    else:
+        print("Erradoo, burroo")
+
+
+
+#while True:
+ #   pergunta = int(input("Escolha um número ?"))
+  #  if pergunta == 20:
+   #     print("Ganhou 100zin")
+    #
+    # else:
+      #  print("Número errado, tente novamente")
